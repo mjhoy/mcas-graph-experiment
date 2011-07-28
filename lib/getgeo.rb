be34da43de0304287ed36347cb3ccc9a27137379
Michael Hoy <michael.john.hoy@gmail.com>
2011-07-28 11:20:21 -0400

    initial script to parse html data, fetch geo data, save as json

---
+++ lib/getgeo.rb
@@ -0,0 +1,93 @@
+require 'uri'
+require 'net/http'
+
+class GeoGet
+
+  BASE_URL = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false"
+
+  class << self
+
+    def geometry_for(address, label)
+      @codes ||= {}
+      return @codes[address] if @codes[address]
+      puts '\n'
+      puts '** Query: ' + label
+      @codes[address] = _fetch(address)
+    end
+
+    def url_for(address)
+      BASE_URL + "&address=" + URI.escape(address)
+    end
+
+    def _fetch(address)
+      url = url_for(address)
+      final = nil
+      while !final
+        res = send_request(url)
+        break unless check_for_ok(res)
+        final = ask(res)
+        url = url_for(ask_for_new_address) unless final
+      end
+      final
+    end
+
+    def send_request(url)
+      res = Net::HTTP.get(URI.parse(url))
+      JSON.parse(res)
+    end
+
+    def ask(res)
+      choice = nil
+      new_address = nil
+      while !choice
+        len = res["results"].length
+        puts "Please choose:"
+        res["results"].each_with_index do |addr, n|
+          puts "#{n+1}. " + addr["formatted_address"]
+        end
+        puts "#{len+1}. Enter a new address for this entry"
+        puts "---"
+
+        a = gets.chomp
+        unless (a.to_i.to_s != a)
+          a = (a.to_i - 1)
+          if a == len
+            break
+          end
+          choice = res["results"][a] if check_maps(res["results"][a])
+        end
+      end
+      choice
+    end
+
+    def check_maps(d)
+      l = d["geometry"]["location"]
+      ll = [l["lat"], l["lng"]].join(",")
+      query = "?ll=#{ll}&z=9&q={ll}"
+      %x{ open http://maps.google.com/#{query} }
+      puts "Enter N if this is correct, else hit return."
+      if gets =~ /^n/i
+        false
+      else
+        true
+      end
+     end
+
+    def ask_for_new_address
+      puts "Please enter a new address:"
+      gets.chomp
+    end
+
+    def check_for_ok(res)
+      if res && res["results"] && res["results"][0] && res["results"][0]["geometry"]
+        true
+      else
+        puts "Request no good:"
+        p res
+        false
+      end
+    end
+
+  end
+
+end
---
+++ script/parse_html_data.rb
@@ -0,0 +1,57 @@
+$: << File.join(File.dirname(__FILE__), '../lib')
+
+require 'nokogiri'
+require 'pp'
+require 'json'
+require 'getgeo'
+
+JSON_FILE_OUTPUT = File.join(File.dirname(__FILE__), '../data/ma_district_geometry.json')
+
+html = File.read(File.join(File.dirname(__FILE__), '../data/mcas_school_grade10_2010_all.html'))
+
+doc = Nokogiri::HTML(html)
+rows = doc.xpath('//table[@id="mcasGridView"]/tr')
+
+# Get headers
+headers = rows[0].xpath('th').map { |h| h.content }
+pp headers
+
+# Add denormalized data for a given MCAS data row `o`
+def denormalize(o)
+  matches = o["Org Name"].match(/(.*)\s-\s(.*)/)
+  district = matches[1]
+  school = matches[2]
+  label = 
+    "DISTRICT: " + district +
+    ", SCHOOL: " + school
+  address = school + ', Massachusetts'
+  new = o.dup
+  new["denorm"] ||= {}
+  new["denorm"]["district"] = district
+  new["denorm"]["school"] = school
+  geom = GeoGet.geometry_for(address, label)
+  new["denorm"]["geometry"] = {
+    "lat" => geom["location"]["lat"],
+    "lng" => geom["location"]["lng"]
+  }
+  new
+end
+
+if File.exist?(JSON_FILE_OUTPUT)
+  data = JSON.parse(File.read(JSON_FILE_OUTPUT))
+else
+  data = []
+end
+
+rows[(data.length + 1)..-1].each do |r|
+  o = {}
+  r.xpath('td').each_with_index do |c, i| 
+    h = headers[i]
+    o[h] = c.content
+  end
+  o = denormalize(o)
+  data.push o
+  json = JSON.pretty_generate(data)
+  File.open(JSON_FILE_OUTPUT, 'w+') { |f| f.write(json) }
+  puts "Wrote file."
+end
